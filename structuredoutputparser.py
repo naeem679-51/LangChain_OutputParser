b0649a3c43b906b767a2c9f00b3be249bb902e86
naeem679-51 <mdnazmulahmed34@gmail.com>
2026-02-08 13:55:26 +0600

    Add files via upload

---
+++ structuredoutputparser.py
@@ -0,0 +1,35 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+
+
+
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+    task='text-generation' 
+    )
+
+model = ChatHuggingFace(llm=llm)
+
+schema = [
+    ResponseSchema(name='fact_1',description='Fact 1 about the topic'),
+    ResponseSchema(name='fact_2',description='Fact 2 about the topic'),
+    ResponseSchema(name='fact_3',description='Fact 3 about the topic')
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+
+template = PromptTemplate(
+    template= 'Give 3 fact about {topic} and following JSON structure \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic':'black hole'})
+
+print(result)
